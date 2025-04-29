fa2e33032e51e14dbbbc5f6742e390b3648f3bdf
EvansTheAuthor <ibnumuhammadab19@gmail.com>
2025-04-29 22:28:03 +0700

    Still a simple API. Need more development.

--- API/doctors.php
+++ API/doctors.php
@@ -6,13 +6,17 @@ $doctors = [
     ['id' => 2, 'name' => 'Dr. Budi', 'specialization' => 'Jantung', 'location' => 'Bandung']
 ];
 
-$filtered = array_filter(array: $doctors, function($doctors){
+$filtered = array_filter($doctors, function($doctors){
     $match = true;
     if (isset($_GET['specialization']) && $_GET['specialization'] !== '') {
        $match = $match && (strpos($doctors['specialization'],$_GET['specialization']) !== false);
     }
-    return $match
+
+    if (isset($_GET['location']) && $_GET['location'] !== '') {
+        $match = $match && (strpos($doctors['location'],$_GET['location']) !== false);
+    }
+    return $match;
 });
 
-echo json_encode(array_values($filtered))
+echo json_encode(array_values($filtered), JSON_PRETTY_PRINT)
 ?>
