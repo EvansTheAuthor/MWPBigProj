8ef66d42fdc827cd613331bcabb6d4b013704625
EvansTheAuthor <ibnumuhammadab19@gmail.com>
2025-04-30 11:12:29 +0700

    creating footer

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<footer class="site-footer">
+    <div class="footer-content">
+        <div class="footer-section">
+            <h3>Quick Links</h3>
+            <ul>
+                <li><a href="index.php?page=home">Beranda</a></li>
+                <li><a href="index.php?page=login">Masuk</a></li>
+                <li><a href="#">Kebijakan Privasi</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-section">
+            <h3>Follow kami</h3>
+        </div>
+    </div>
+</footer>
