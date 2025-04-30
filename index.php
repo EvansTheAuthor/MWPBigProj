579b01a68456b6813d71ba93a525e0049efc983f
EvansTheAuthor <ibnumuhammadab19@gmail.com>
2025-04-30 11:05:15 +0700

    modifying main page

--- index.php
+++ index.php
@@ -7,8 +7,12 @@
     <script>"https://unpkg.com/vue@3/dist/vue.global.js"</script>
 </head>
 <body>
+    <?php include 'includes/header.php?';?>
+
     <main>
-        
+        <?php include 'pages/{$page}.php';?>
     </main>
+
+    <?php include 'includes/footer.php'; ?>
 </body>
 </html>
