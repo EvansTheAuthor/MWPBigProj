4796ffeeab83b21b0853db90880896f6cea42b48
EvansTheAuthor <ibnumuhammadab19@gmail.com>
2025-04-30 11:25:26 +0700

    Footer of the site

--- includes/footer.php
+++ includes/footer.php
@@ -10,7 +10,22 @@
         </div>
 
         <div class="footer-section">
-            <h3>Follow kami</h3>
+            <h3>Kontak Kami</h3>
+            <p>Email: support@konsuldokter.com</p>
+            <p>Telepon +62123-456-7890</p>
         </div>
+
+        <div class="footer-section">
+            <h3>Follow Kami</h3>
+            <div class="social-links">
+                <a href="#" class="social-icon"><img src="/assets/images/facebook-svgrepo-com.svg" alt="Facebook"></a>
+                <a href="#" class="social-icon"><img src="/assets/images/icons8-twitter.svg" alt="Twitter"></a>
+                <a href="#" class="social-icon"><img src="/assets/images/instagram-svgrepo-com.svg" alt="Instagram"></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright">
+        <p>&copy; <?php echo date('Y');?>KonsulDok. All rights reserved.</p>
     </div>
 </footer>
